ea0cead34c3d44f5baa9b1c40c1c13c6224170d0
sangbin jeong <jsngbin@gmail.com>
2024-03-27 23:45:27 +0900

    changed 34.jpg to bottom.jpg, fix wedding date count, font size

--- js/main.js
+++ js/main.js
@@ -252,7 +252,7 @@
 
 
 	// Set the date we're counting down to
-		var countDownDate = new Date("Mar 11, 2023 16:20:00").getTime();
+		var countDownDate = new Date("Jun 2, 2024 15:20:00").getTime();
 
 		// Update the count down every 1 second
 		var x = setInterval(function() {
